9d39f9ff5a5e5d7563d73213f53f847a20990a9e
Leon <liu1968759337@gmail.com>
2024-08-29 16:35:03 +0000

    updated slab prepare with prepare vaccum layer convergence function; Cu_test file added for test run, to be replaced by real castep file

--- pseudopotential_calculator/calculations/slab.py
+++ pseudopotential_calculator/calculations/slab.py
@@ -1,19 +1,13 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any, Self, cast, overload
-
-import numpy as np
-from ase import Atoms
+from typing import TYPE_CHECKING, Self
 
 from pseudopotential_calculator.castep import CastepConfig, get_default_calculator
-from pseudopotential_calculator.util import get_figure
 
 if TYPE_CHECKING:
+    from ase import Atoms
     from ase.calculators.castep import Castep
-    from matplotlib.axes import Axes
-    from matplotlib.figure import Figure
-    from matplotlib.lines import Line2D
 
 
 @dataclass
@@ -26,7 +20,7 @@ class SlabOptimizationParams:
 
     @property
     def kpoint_mp_grid(self: Self) -> str:
-        return f"{self.n_k_points} {self.n_k_points} {self.n_k_points}"
+        return f"{self.n_k_points} {self.n_k_points} {1}"
 
 
 def get_slab_optimization_calculator(
@@ -38,8 +32,12 @@ def get_slab_optimization_calculator(
 
     calculation.param.xc_functional = parameters.xc_functional
     calculation.param.cut_off_energy = parameters.cut_off_energy
-
+    calculation.param.spinpolarised = "true"
+    calculation.param.elec_energy_tol = 1.000000000000000e-06
+    calculation.param.geom_energy_tol = 1.000000000000000e-05
+    calculation.param.geom_disp_tol = 1.000000000000000e-03
     calculation.cell.kpoint_mp_grid = parameters.kpoint_mp_grid
+
     # Prevent the bulk cell from rotating
     calculation.cell.cell_constraints = "1 1 1\n0 0 0"
     calculation.task = "GeometryOptimization"
@@ -48,70 +46,3 @@ def get_slab_optimization_calculator(
     # Temporary fix for bug in ase
     atoms.calc = calculation
     return calculation
-
-
-def _plot_cell_length_against_n_k_points(
-    n_k_points: np.ndarray[Any, np.dtype[np.float64]],
-    bond_lengths: np.ndarray[Any, np.dtype[np.float64]],
-    *,
-    ax: Axes | None = None,
-) -> tuple[Figure, Axes, Line2D]:
-    fig, ax = get_figure(ax)
-
-    args = np.argsort(n_k_points)
-    (line,) = ax.plot(n_k_points[args], bond_lengths[args])  # type: ignore library
-    ax.set_xlabel("number of k-points")  # type: ignore library
-    ax.set_ylabel(r"Cell Length / $\AA$")  # type: ignore library
-    ax.set_title("Plot of cell length vs number of k-points")  # type: ignore library
-    return fig, ax, line
-
-
-def _plot_energy_against_n_k_points(
-    n_k_points: np.ndarray[Any, np.dtype[np.float64]],
-    energy: np.ndarray[Any, np.dtype[np.float64]],
-    *,
-    ax: Axes | None = None,
-) -> tuple[Figure, Axes, Line2D]:
-    fig, ax = get_figure(ax)
-
-    args = np.argsort(n_k_points)
-    (line,) = ax.plot(n_k_points[args], energy[args])  # type: ignore library
-    ax.set_xlabel("number of k-points")  # type: ignore library
-    ax.set_ylabel("Energy / eV")  # type: ignore library
-    ax.set_title("Plot of energy vs number of k-points")  # type: ignore library
-    return fig, ax, line
-
-
-def _get_cell_lengths_from_calculator(
-    calculator: Castep,
-) -> tuple[float, ...]:
-    return cast(Atoms, calculator.atoms).get_cell_lengths_and_angles()[0:3]  # type: ignore unknown
-
-
-@overload
-def _get_n_k_points_from_calculator(
-    calculator: Castep,
-    direction: None = None,
-) -> tuple[int, ...]:
-    ...
-
-
-@overload
-def _get_n_k_points_from_calculator(
-    calculator: Castep,
-    direction: int,
-) -> int:
-    ...
-
-
-def _get_n_k_points_from_calculator(
-    calculator: Castep,
-    direction: int | None = None,
-) -> tuple[int, ...] | int:
-    kpoint_mp_grid = cast(
-        tuple[int, ...],
-        calculator.cell.kpoint_mp_grid.raw_value,  # type: ignore unkown
-    )
-    if direction is None:
-        return kpoint_mp_grid
-    return kpoint_mp_grid[direction]
--- scripts/copper/slab/prepare.py
+++ scripts/copper/slab/prepare.py
@@ -1,10 +1,46 @@
-from ase.build import surface
-from ase.io import read, write
+from pathlib import Path, PosixPath
+
+from ase import Atoms
+from ase.build import (
+    surface,  # type: ignore  # noqa: PGH003
+)
+from ase.io import read  # type: ignore  # noqa: PGH003
+
+from pseudopotential_calculator.calculations.slab import (
+    SlabOptimizationParams,
+    get_slab_optimization_calculator,
+)
+from pseudopotential_calculator.castep import Castep, CastepConfig
+from pseudopotential_calculator.hpc import (
+    copy_files_to_hpc,
+    prepare_calculator_with_submit_script,
+    prepare_submit_all_script,
+)
+from pseudopotential_calculator.util import prepare_clean_directory
+
+
+def _prepare_vaccum_layer_convergence(atom: Atoms, data_path: Path) -> None:
+    calculators = list[Castep]()
+    prepare_clean_directory(data_path)
+
+    for n_vaccum_layer in range(3, 21):
+        slab_copper = surface(atom, (1, 1, 0), 5, vacuum=n_vaccum_layer)  # type: ignore
+        config = CastepConfig(
+            data_path / f"slab_{n_vaccum_layer}_vaccum_layer",
+            f"slab_{n_vaccum_layer}_vaccum_layer",
+        )  # type: ignore
+        params = SlabOptimizationParams(n_k_points=10)
+        calculator = get_slab_optimization_calculator(slab_copper, params, config)  # type: ignore
+        prepare_calculator_with_submit_script(calculator)
+
+        calculators.append(calculator)  # type: ignore
+    prepare_submit_all_script(calculators, data_path)
+    copy_files_to_hpc(data_path, PosixPath(data_path.as_posix()))
+
+
+VACUUM_LAYER_PATH = Path("data/copper/slab/vaccum_layer")  # type: ignore
 
 if __name__ == "__main__":
-    bulk_copper = read("/Users/tychus/summer_project/Cu.cell", format="castep-cell")
-    slab_copper = surface(bulk_copper, (0, 0, 1), 6, vacuum=3)
+    bulk_copper = read("Cu_test.poscar", format="vasp")
     # write the model file to be viewed
-    write("/Users/tychus/summer_project/Cu_out.cell", b, format="castep-cell")
-
-    _prepare_vaccum_layer_convergence(slab_copper, K_POINTS_PATH_WC)
+    _prepare_vaccum_layer_convergence(bulk_copper, VACUUM_LAYER_PATH)  # type: ignore
