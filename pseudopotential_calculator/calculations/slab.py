7a808f5f0310175c8241dda8d5972b18d526ab85
Leon <liu1968759337@gmail.com>
2024-09-05 13:43:24 +0000

    update

--- pseudopotential_calculator/calculations/slab.py
+++ pseudopotential_calculator/calculations/slab.py
@@ -8,7 +8,7 @@ from ase.build import (
     add_vacuum,  # type: ignore bad library
     surface,  # type: ignore bad library
 )
-from ase.constraints import FixAtoms
+from ase.constraints import FixAtoms, FixedLine
 
 from pseudopotential_calculator.calculations.generic import OptimizationParamsBase
 from pseudopotential_calculator.castep import (
@@ -79,6 +79,24 @@ def _get_height_per_layer(
     return np.abs(cast(float, position_0) - cast(float, position_1))
 
 
+def _get_constraints(
+    atoms: Atoms,
+    n_fixed_layer: int,
+    height_per_layer: float,
+    slab_direction: tuple[int, int, int],
+) -> list[FixAtoms | FixedLine]:
+    fixed_layer_mask = [i < n_fixed_layer for i in range(len(atoms))]
+    free_layer_indices = [  # type: ignore bad lib
+        atom.index  # type: ignore bad lib
+        for atom in atoms  # type: ignore bad lib
+        if atom.position[2] >= n_fixed_layer * height_per_layer  # type: ignore bad lib
+    ]
+    return [
+        FixAtoms(fixed_layer_mask),
+        FixedLine(free_layer_indices, slab_direction),  # type: ignore bad lib
+    ]
+
+
 def get_surface(
     atom: Atoms,
     slab_direction: tuple[int, int, int],
@@ -93,22 +111,38 @@ def get_surface(
     height_per_layer = _get_height_per_layer(atom, slab_direction)
     add_vacuum(atoms, n_vacuum_layer * height_per_layer)
 
-    # TODO: fix the first n_fixed atoms, and fix the x,y coordinate of the
-    # next n_free atoms
-    atoms.set_constraint(FixAtoms(mask=[True for _ in atoms]))
+    atoms.set_constraint(  # type: ignore bad lib
+        _get_constraints(atoms, n_fixed_layer, height_per_layer, slab_direction),
+    )
     return atoms
 
 
 def _get_n_vacuum_layers_from_slab(atom: Atoms) -> float:
     cell_height = atom.get_cell().array[-1][-1]
 
-    atom_heights = list[float](atom.positions[:, 2])  # type: ignore unknown
-    slab_height = max(atom_heights) - min(atom_heights)
-    height_per_layer = slab_height / len(atom_heights)
+    slab_height = get_n_th_slab_layer_height(atom, 1)
+    height_per_layer = get_height_per_slab_layer(atom)
 
     vacuum_height = cell_height - slab_height
 
     return vacuum_height / height_per_layer
+
+
+def get_n_th_slab_layer_height(atom: Atoms, n_th_layer: int) -> float:
+    heights = cast(list[float], atom.positions[:, 2])  # type: ignore bad lib
+    sorted_heights = np.unique(heights)[::-1]
+    return sorted_heights[n_th_layer - 1]
+
+
+def get_height_per_slab_layer(atom: Atoms) -> float:
+    heights = cast(list[float], atom.positions[:, 2])  # type: ignore bad lib
+    sorted_heights = np.unique(heights)[::-1]
+    return sorted_heights[-2]
+
+
+def get_n_slab_layer(atom: Atoms) -> int:
+    heights = cast(list[float], atom.positions[:, 2])  # type: ignore bad lib
+    return len(heights)
 
 
 def plot_energy_against_n_vacuum_layer(
@@ -137,12 +171,10 @@ def plot_energy_against_n_vacuum_layer(
 
 
 def get_atom_displacement(atom: Atoms, n_th_layer: int) -> float:
-    heights = cast(list[float], atom.positions[:, 2])  # type: ignore bad lib
-    n_cu_layer = len(np.unique(heights))
-    sorted_heights = np.unique(heights)[::-1]
-    atom_height = sorted_heights[n_th_layer - 1]
-    height_per_fixed_layer = sorted_heights[-2]
-    atom_height_before_relax = (n_cu_layer - (n_th_layer - 1)) * height_per_fixed_layer
+    n_slab_layer = get_n_slab_layer(atom)
+    atom_height = get_n_th_slab_layer_height(atom, n_th_layer)
+    height_per_slab_layer = get_height_per_slab_layer(atom)
+    atom_height_before_relax = (n_slab_layer - (n_th_layer - 1)) * height_per_slab_layer
     return atom_height_before_relax - atom_height
 
 
@@ -177,5 +209,4 @@ def plot_displacement_against_n_free_layer(
         ("Displacement", np.array(displacement), "m"),
         ax=ax,
     )
-    line.set_label(f"layer {layer_idx}")
     return fig, ax, line
